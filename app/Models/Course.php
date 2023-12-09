642d90bf09d3abce1ac70fe6041c485c752bf537
IcegnawerTest <150777415+IcegnawerTest@users.noreply.github.com>
2023-12-09 10:41:06 +0500

    Update Course.php

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -15,4 +15,11 @@ class Course extends Model
         "image",
         "category_id"
     ];
+
+    /* 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    */
 }
